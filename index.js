58c13170299f3280f81bb7aec67c2c5398955e73
kudou-reira <h.i.y.e.o.XD@gmail.com>
2017-08-29 21:04:41 +0900

    make it better

--- index.js
+++ index.js
@@ -43,11 +43,6 @@ app.get('/latest', (req, res) => {
 
 if(process.env.NODE_ENV === 'production'){
 	app.use(express.static('client/build'));
-
-	const path = require('path');
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
 }
 
 const port = process.env.PORT || 5000
